74152ff7c17a9fe340f98733f713a183131147ad
fathimabuniyam <fathimaob@gmail.com>
2025-11-27 02:01:34 +0300

    chore: expo app boilerplate replaced with required files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,42 @@
+import { HapticTab } from '@/components/ui/HapticTab';
+import Colors from '@/constants/Colors.constants';
+import { t } from '@/i18n';
+import Icon from '@react-native-vector-icons/material-design-icons';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors.PRIMARY,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: t('home'),
+          tabBarIcon: ({ color, focused }) => (
+            <Icon
+              size={28}
+              name={focused ? 'home' : 'home-outline'}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="profile"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: t('profile'),
+          tabBarIcon: ({ color, focused }) => (
+            <Icon
+              size={28}
+              name={focused ? 'account' : 'account-outline'}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
